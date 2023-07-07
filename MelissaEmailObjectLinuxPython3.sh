5b4a7cfcbd823f36ba8d7336380f0f7ef398581a
Melissa <>
2023-07-07 09:58:58 -0700

    Tested successfully using Melissa data files for 2023.06

--- MelissaEmailObjectLinuxPython3.sh
+++ MelissaEmailObjectLinuxPython3.sh
@@ -42,7 +42,7 @@ while [ $# -gt 0 ] ; do
 done
 
 # ######################### Config ###########################
-RELEASE_VERSION='2023.05'
+RELEASE_VERSION='2023.06'
 ProductName="DQ_EMAIL_DATA"
 
 # Uses the location of the .sh file 
